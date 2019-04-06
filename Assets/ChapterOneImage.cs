2f0e27872e86dca8a0555e5623955f226f1db7ef
Stella Cannefax <stella@unity3d.com>
2019-04-05 23:11:05 -0700

    initial commit with chapter one

---
+++ Assets/ChapterOneImage.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+
+public class ChapterOneImage
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Color24
+    {
+        public byte r;
+        public byte g;
+        public byte b;
+
+        public Color24(byte r, byte g, byte b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
+
+    public struct Job : IJob
+    {
+        public int2 size;
+
+        [WriteOnly]
+        public NativeArray<Color24> Pixels;
+
+        public void Execute()
+        {
+            var nx = (float) size.x;
+            var ny = (float) size.y;
+            for (float j = 0; j < size.y; j++)
+            {
+                for (float i = 0; i < size.x; i++)
+                {
+                    const float rgbMultiplier = 255.999f;
+                    byte r = (byte)(i / nx * rgbMultiplier);
+                    byte g = (byte)(j / ny * rgbMultiplier);
+                    byte b = (byte)((0.2f) * rgbMultiplier);
+
+                    var index = (int) (j * nx + i);
+                    
+                    Pixels[index] = new Color24(r, g, b);
+                }
+            }
+        }
+    }
+
+    static readonly int2 imageSize = new int2(200, 100);
+
+    public JobHandle Handle;
+
+    public Texture2D texture = new Texture2D(imageSize.x, imageSize.y, TextureFormat.RGB24, false);
+    
+    public void WriteTestImage()
+    {
+        var buffer = new NativeArray<Color24>(imageSize.x * imageSize.y, Allocator.Persistent);
+
+        var job = new Job()
+        {
+            size = imageSize,
+            Pixels = buffer
+        };
+
+        Handle = job.Schedule();
+        Handle.Complete();
+        
+        texture.LoadRawTextureData(job.Pixels);
+        texture.Apply();
+        
+        buffer.Dispose();
+    }
+}
---
+++ Assets/Editor/WeekendWindow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WeekendWindow : EditorWindow
+{
+    ChapterOneImage m_ChapterOne;
+
+    [MenuItem("Window/Tracer")]
+    public static void ShowWindow()
+    {
+        var window = GetWindow<WeekendWindow>();
+        window.Show();
+    }
+
+    void OnEnable()
+    {
+        m_ChapterOne = new ChapterOneImage();
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Draw Chapter One Test Image"))
+        {
+            m_ChapterOne.WriteTestImage();
+        }
+
+        var rect = EditorGUILayout.GetControlRect(GUILayout.Width(200), GUILayout.Height(100));
+        EditorGUI.DrawPreviewTexture(rect, m_ChapterOne.texture);
+    }
+}
